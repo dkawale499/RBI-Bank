4bf04dcdab95d097d6fa7505924ef9d1b71d5c71
dkawale499 <dkawale499@gmail.com>
2024-01-15 19:27:17 +0530

    Bank of barda added

---
+++ RBI-Bank/src/main/java/com/rbi/baroda/BarodaBank.java
@@ -0,0 +1,12 @@
+package com.rbi.baroda;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BarodaBank 
+{
+	public BarodaBank()
+	{
+		System.out.println("Baroda Bank Added");
+	}
+}
