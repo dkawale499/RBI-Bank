b81b060a5087819a81060f4d3307e81e7ecd2e6e
dkawale499 <dkawale499@gmail.com>
2024-01-15 19:17:53 +0530

    project added

---
+++ RBI-Bank/src/main/java/com/rbi/RbiBankApplication.java
@@ -0,0 +1,13 @@
+package com.rbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbiBankApplication.class, args);
+	}
+
+}
---
+++ RBI-Bank/src/test/java/com/rbi/RbiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
